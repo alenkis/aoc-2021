60430669d805e693357eef4f3476b8a87ce5a936
Alen Kis <akis@tempest.local>
2021-12-01 23:31:19 -0500

    use traverse ZipList instead of zip/zip3

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,16 +1,29 @@
 module Day01 where
 
+import Control.Applicative
+import Data.List
+
+windows :: Int -> [a] -> [[a]]
+windows n = getZipList . traverse ZipList . take n . tails
+
 countIncreases :: [Int] -> Int
-countIncreases input = sum $ zipWith (curry fn) (tail input) input
+countIncreases input = sum $ map fn (windows 2 input)
   where
-    fn (f, s)
-      | f > s = 1
+    fn [f, s]
+      | s > f = 1
       | otherwise = 0
+    fn _ = 0
 
 rollingMeasurement :: [Int] -> [Int]
 rollingMeasurement input = map fn $ zip3 (tail (tail input)) (tail input) input
   where
     fn (f, s, t) = f + s + t
+
+rollingMeasurement' :: [Int] -> [Int]
+rollingMeasurement' input = map fn $ windows 3 input
+  where
+    fn [f, s, t] = f + s + t
+    fn _ = 0
 
 day01 :: IO ()
 day01 = do
